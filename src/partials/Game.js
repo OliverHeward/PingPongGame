f4ebba1d581643caed6daa460b0903f1175fbaef
Oliver Heward <oliverhewardbie@gmail.com>
2019-02-07 16:56:52 +0000

    Pong ggame board created through class and rendered

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,13 +1,35 @@
+import { SVG_NS } from '../settings';
+import Board from './board';
+
 export default class Game {
-  constructor(element, width, height) {
-    this.element = element;
-    this.width = width;
-    this.height = height;
+    constructor(element, width, height) {
+        this.element = element;
+        this.width = width;
+        this.height = height;
 
-		// Other code goes here...
-  }
+        this.gameElement = document.getElementById(this.element);
 
-  render() {
-		// More code goes here....
-  }
+        // Creates a board object and set width and height to game width and height
+
+        this.board = new Board(this.width, this.height);
+    }
+
+    render() {
+        // create svg element
+        this.gameElement.innerHTML = '';
+
+        let svg = document.createElementNS(SVG_NS, 'svg');
+
+        // set attributes 
+        svg.setAttributeNS(null, 'width', this.width);
+        svg.setAttributeNS(null, 'height', this.height);
+        svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
+        // append SVG too game element by Id
+        this.gameElement.appendChild(svg);
+
+        //render the game components
+        this.board.render(svg);
+        this.board.render(svg);
+
+    }
 }
---
+++ src/settings.js
@@ -0,0 +1,3 @@
+// Settings.js 
+
+export const SVG_NS = "http://www.w3.org/2000/svg";
